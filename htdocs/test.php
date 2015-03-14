f4a1226e9aecdf015b1baa22b9c750d4daa5c985
xnrand <xnrand@gmail.com>
2015-03-14 18:20:49 +0100

    [test.php] technical note

--- htdocs/test.php
+++ htdocs/test.php
@@ -2,4 +2,6 @@
 // You should see this PHP code instead of just "It works!" since PHP should be disabled for security reasons.
 echo "It works!";
 // 13-11-12
+
+// Note: Do NOT delete this file. --xnrand 15-03-14
 ?>
